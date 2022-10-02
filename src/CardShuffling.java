81c2cb6210868e372e4fb929d6c551495f66a627
CaptMD-11 <86084719+CaptMD-11@users.noreply.github.com>
2022-10-01 20:10:43 -0700

    another commit

--- src/CardShuffling.java
+++ src/CardShuffling.java
@@ -6,38 +6,54 @@ public class CardShuffling {
 
     }
 
-    private boolean isOriginalOrder(String[][] originalDeck, ArrayList<String[]> curr) {
-        for (int i = 0; i < originalDeck.length; i++) {
-            for (int j = 0; j < originalDeck[i].length; j++) {
-                if (originalDeck[i][j].equals(curr.get(i)[j]) == false)
-                    return false;
-            }
+    private static boolean isOriginalOrder(ArrayList<Integer> originalDeck, ArrayList<Integer> curr) {
+        for (int i = 0; i < originalDeck.size(); i++) {
+            if (originalDeck.get(i) == curr.get(i) == false)
+                return false;
         }
         return true;
     }
 
-    public int getNumberOfRounds(ArrayList<String[]> deck) {
+    public static void printDeck(ArrayList<Integer> deck) {
+        for (int i = 0; i < deck.size(); i++) {
+            System.out.print(deck.get(i) + "  ");
+        }
+        System.out.println();
+    }
+
+    public static int getNumberOfRounds(ArrayList<Integer> deck) {
         boolean goesToTable = true;
         int count = 0;
-        String[][] originalDeck = new String[deck.size()][2];
+        ArrayList<Integer> originalDeck = new ArrayList<Integer>();
 
         for (int i = 0; i < deck.size(); i++) {
-            originalDeck[i][0] = deck.get(i)[0];
-            originalDeck[i][1] = deck.get(i)[1];
+            originalDeck.add(deck.get(i));
         }
 
-        ArrayList<String[]> table = new ArrayList<String[]>();
+        ArrayList<Integer> table = new ArrayList<Integer>();
+        int deckSize = deck.size();
 
-        while (isOriginalOrder(originalDeck, deck) == false) {
-            for (int i = 0; i < deck.size(); i++) {
-                if (goesToTable == true)
-                    table.add(deck.get(i));
-                else
-                    deck.add(deck.remove(i));
+        do {
+
+            printDeck(deck);
+
+            while (deckSize != 0) {
+                if (goesToTable == true) {
+                    table.add(0, deck.remove(0));
+                    deckSize--;
+                } else {
+                    deck.add(deck.remove(0));
+                }
                 goesToTable = !goesToTable;
             }
+            for (int i = 0; i < table.size(); i++) {
+                deck.add(table.get(i));
+            }
+            table = new ArrayList<Integer>(1);
+            deckSize = table.size();
             count++;
-        }
+        } while (isOriginalOrder(originalDeck, deck) == false);
+
         return count;
     }
 
--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class Runner {
     public static void main(String[] args) {
+
+        ArrayList<Integer> deck = new ArrayList<Integer>();
+
+        deck.add(1);
+        deck.add(2);
+        deck.add(3);
+        // deck.add(4);
+        // deck.add(5);
+
+        System.out.println(CardShuffling.getNumberOfRounds(deck));
 
     }
 }
